4987b412399030f4c803339d8ef7811ab82574eb
Luti <lutians@hotmail.com>
2022-08-31 16:37:12 -0300

    Primer commit

---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dólar de Estados Unidos'},
+    { id: 'ARS', nombre: 'Peso argentino'},
+    { id: 'MXN', nombre: 'Peso mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Libra esterlina'},
+    
+]
+
+export {
+    monedas
+}
